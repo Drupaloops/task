b5e6776716153e3e9d1687da1dae499088d01596
Drupaloops <opduhao@gmail.com>
2018-06-10 23:51:28 +0800

    router

--- src/route/index.js
+++ src/route/index.js
@@ -1,7 +1,9 @@
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 
+import Hello from '../view/hello'
 const AppRouter = () => (
     <BrowserRouter>
-        
+        <Route path="/test" component={Hello} />
     </BrowserRouter>
 )
+export default AppRouter
