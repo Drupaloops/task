4619186dee3b68f2091baa7f13d69f9a77fe370c
Drupaloops <opduhao@gmail.com>
2018-06-13 23:29:47 +0800

    router

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 import Hello from './pages/hello'
 import About from './pages/about'
 
 
+
 ReactDOM.render(
-    <BrowserRouter
-        // basename="/oops"
-        // forceRefresh={false}
-        // getUserConfirmation={getConfirmation()}
-        // keyLength={12}
-    >
+    <BrowserRouter>
         <div>
             {/* react-router 内只允许存在一个子元素，所以用div包裹 */}
+            <ul>
+                <li><Link to="/">home</Link></li>
+                <li><Link to="/helo">helo</Link></li>
+            </ul>
             <Route exact path="/" component={Hello} />
             <Route path="/helo" component={About} />
         </div>
--- src/route/index.js
+++ src/route/index.js
@@ -1,6 +1,21 @@
 import { BrowserRouter, Route } from 'react-router-dom'
 
 import Hello from '../view/hello'
+
+
+const routes = [
+    {
+        path: '/',
+        exact: true,
+        component: Hello
+    },
+    {
+        path: '/about',
+        component: About
+    }
+]
+
+
 const AppRouter = () => (
     <BrowserRouter>
         <Route path="/test" component={Hello} />
