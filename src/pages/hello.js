da576dd282e1bd78f76a6500204503388a927092
Drupaloops <opduhao@gmail.com>
2018-07-10 23:25:28 +0800

    task

--- src/pages/hello.js
+++ src/pages/hello.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import TaskList from '../components/taskList'
 
 class Hello extends React.Component {
     render () {
         return (
             <React.Fragment>
                 <h1>task center</h1>
+                <p>123</p>
                 {/* <Group /> */}
-                {/* <RiskList /> */}
+                <TaskList />
             </React.Fragment>
         )
     }
